8f3751a852067e8fc22805905b443b4bebb1153f
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-11-15 12:14:41 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::arch::asm;
 use std::env;
 use std::io::Read;
+use std::ops::Deref;
 
+pub struct ProcessStartInfo {
+    dummy: u64,
+    debugPrint: extern "win64" fn(String),
+}
 fn main() {
     {
         println!("{}", std::usize::MAX);
-        let args: Vec<String> = env::args().collect();
-        println!("Hello, world!");
         let mut filePath = String::new();
+        let args: Vec<String> = env::args().collect();
         if (args.len() > 1) {
             filePath = args[1].clone();
         } else if (env::var("EXE").is_ok()) {
@@ -15,51 +19,53 @@ fn main() {
         } else {
             return;
         }
+        let function_pointer = loadExecutable(filePath);
+        let info = Box::new(ProcessStartInfo {
+            dummy: 123,
+            debugPrint: debugPrint,
+        });
+        let infoPrtr = Box::into_raw(info);
+        let ret = function_pointer(infoPrtr);
 
-        println!("filePath, {}!", filePath);
-        //open file stream
-        let mut file = std::fs::File::open(&filePath).unwrap();
-        //read binary whole file
-        let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer).unwrap();
-        //print file contents
-
-        use windows_sys::Win32::System::Memory::{
-            VirtualAlloc, MEM_COMMIT, MEM_RESERVE,
-            PAGE_EXECUTE_READWRITE
-        };
-
-        let size: usize = 4096;
-        let ptr = unsafe {
-            VirtualAlloc(
-                std::ptr::null_mut(),
-                size,
-                MEM_COMMIT | MEM_RESERVE,
-                PAGE_EXECUTE_READWRITE,
-            )
-        };
-        assert!(!ptr.is_null());
-        //copy buffer to ptr
-        let buffer_executable=ptr as *mut u8;
-        unsafe {
-            std::ptr::copy_nonoverlapping(buffer.as_ptr(), buffer_executable, buffer.len());
-        }
-
-//cast buffer_executable to function pointer
-        let function_pointer = unsafe {
-            std::mem::transmute::<*mut u8, fn()>(buffer_executable)
-        };
-
-
-        function_pointer();
-        // unsafe {
-        //     asm! {
-        //     "jmp rax",
-        //     in("rax") buffer_executable,
-        //     options(noreturn)
-        //     };
-        // }
-
-        println!("Execution ended");
+        println!("Execution ended with return value: {}", ret);
     }
+}
+fn loadExecutable(filePath: String) -> extern "win64" fn(*mut ProcessStartInfo) -> u64 {
+    println!("Hello, world!");
+
+    println!("filePath, {}!", filePath);
+    //open file stream
+    let mut file = std::fs::File::open(&filePath).unwrap();
+    //read binary whole file
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    //print file contents
+
+    use windows_sys::Win32::System::Memory::{
+        VirtualAlloc, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE,
+    };
+
+    let size: usize = 1024 * 1024;
+    let ptr = unsafe {
+        VirtualAlloc(
+            std::ptr::null_mut(),
+            size,
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_EXECUTE_READWRITE,
+        )
+    };
+    assert!(!ptr.is_null());
+    //copy buffer to ptr
+    let buffer_executable = ptr as *mut u8;
+    unsafe {
+        std::ptr::copy_nonoverlapping(buffer.as_ptr(), buffer_executable, buffer.len());
+    }
+
+    let function_pointer: extern "win64" fn(*mut ProcessStartInfo) -> u64 = unsafe {
+        std::mem::transmute::<*mut u8, extern "win64" fn(*mut ProcessStartInfo) -> u64>(buffer_executable)
+    };
+    return function_pointer;
+}
+extern "win64" fn debugPrint(x: String) {
+    println!("{}",x);
 }
