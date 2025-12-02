4816a67ebafbc0f1a2c4fbf5213432a208380b5f
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-12-02 23:51:06 +0100

    working

---
+++ src/elf_parser.rs
@@ -0,0 +1,115 @@
+use std::io::Read;
+use std::mem;
+
+pub struct ElfParser {}
+
+impl ElfParser {
+    pub(crate) fn parse(buffer: &[u8]) -> Vec<ElfSection> {
+        // ELF header
+        let header_buffer = &buffer[0..mem::size_of::<Elf64Ehdr>()];
+        let header: &Elf64Ehdr = unsafe { &*(header_buffer.as_ptr() as *const Elf64Ehdr) };
+
+        if &header.e_ident[0..4] != b"\x7FELF" {
+            panic!("Not an ELF file");
+        }
+
+        // Sekcja z nazwami
+        let name_offset =
+            header.e_shoff as usize + (header.e_shstrndx as usize * header.e_shentsize as usize);
+        let name_buffer = &buffer[name_offset..name_offset + mem::size_of::<Elf64Shdr>()];
+        let shstr: &Elf64Shdr = unsafe { &*(name_buffer.as_ptr() as *const Elf64Shdr) };
+
+        // Wczytaj string table
+        let mut strtab =
+            &buffer[shstr.sh_offset as usize..shstr.sh_offset as usize + shstr.sh_size as usize];
+        let mut sections = Vec::new();
+        // Iteruj po sekcjach
+        for i in 0..header.e_shnum {
+            let offset = header.e_shoff as usize + (i as usize * header.e_shentsize as usize);
+            let buf_section = &buffer[offset..offset + mem::size_of::<Elf64Shdr>()];
+            let shdr: &Elf64Shdr = unsafe { &*(buf_section.as_ptr() as *const Elf64Shdr) };
+
+            let name_offset = shdr.sh_name as usize;
+            let name = strtab[name_offset..].split(|&c| c == 0).next().unwrap();
+            let name_str = String::from_utf8_lossy(name);
+            println!("Section {}: {}", i, name_str);
+            sections.push(ElfSection {
+                name: name_str.to_string(),
+                data: Vec::from(
+                    &buffer
+                        [shdr.sh_offset as usize..shdr.sh_offset as usize + shdr.sh_size as usize],
+                ),
+                header: shdr.clone(),
+            });
+        }
+        return sections;
+    }
+}
+
+impl ElfParser {}
+
+#[repr(C)]
+#[derive(Debug)]
+struct Elf64Ehdr {
+    e_ident: [u8; 16], // Magic number and other info
+    e_type: u16,       // Object file type
+    e_machine: u16,    // Architecture
+    e_version: u32,    // Object file version
+    e_entry: u64,      // Entry point virtual address
+    e_phoff: u64,      // Program header table file offset
+    e_shoff: u64,      // Section header table file offset
+    e_flags: u32,      // Processor-specific flags
+    e_ehsize: u16,     // ELF header size in bytes
+    e_phentsize: u16,  // Program header table entry size
+    e_phnum: u16,      // Program header table entry count
+    e_shentsize: u16,  // Section header table entry size
+    e_shnum: u16,      // Section header table entry count
+    e_shstrndx: u16,   // Section header string table index
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct Elf64Shdr {
+    pub sh_name: u32,
+    pub sh_type: u32,
+    pub sh_flags: u64,
+    pub sh_addr: u64,
+    pub sh_offset: u64,
+    pub sh_size: u64,
+    pub sh_link: u32,
+    pub sh_info: u32,
+    pub sh_addralign: u64,
+    pub sh_entsize: u64,
+}
+
+pub struct ElfSection {
+    pub name: String,
+    pub data: Vec<u8>,
+    pub header: Elf64Shdr,
+}
+impl Clone for ElfSection {
+    fn clone(&self) -> Self {
+        Self {
+            name: self.name.clone(),
+            data: self.data.clone(),
+            header: self.header.clone(),
+        }
+    }
+}
+
+impl Clone for Elf64Shdr {
+    fn clone(&self) -> Self {
+        Self {
+            sh_name: self.sh_name,
+            sh_type: self.sh_type,
+            sh_flags: self.sh_flags,
+            sh_addr: self.sh_addr,
+            sh_offset: self.sh_offset,
+            sh_size: self.sh_size,
+            sh_link: self.sh_link,
+            sh_info: self.sh_info,
+            sh_addralign: self.sh_addralign,
+            sh_entsize: self.sh_entsize,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::arch::asm;
-use std::{env, mem};
+use crate::elf_parser::{Elf64Shdr, ElfParser};
 use std::alloc::{GlobalAlloc, Layout};
+use std::any::Any;
+use std::arch::asm;
+use std::cell::RefCell;
+use std::hash::Hash;
 use std::io::Read;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
+use std::ptr::read_volatile;
+use std::rc::Rc;
+use std::{env, mem};
+
+mod elf_parser;
 
 pub struct ProcessStartInfo {
     dummy: u64,
@@ -22,16 +30,16 @@ fn main() {
             return;
         }
         let function_pointer = loadExecutable(filePath);
-        extern "win64" fn debugPrint(x:&str){
-            println!("{}",x);
+        extern "win64" fn debugPrint(x: &str) {
+            println!("{}", x);
         }
-        extern "win64" fn debugPrintInt(x:u64){
-            println!("{}",x);
+        extern "win64" fn debugPrintInt(x: u64) {
+            println!("{}", x);
         }
         let info = Box::new(ProcessStartInfo {
             dummy: 123,
             debugPrint: debugPrint,
-            debugPrintInt: debugPrintInt
+            debugPrintInt: debugPrintInt,
         });
         let infoPrtr = Box::into_raw(info);
         let ret = function_pointer(infoPrtr);
@@ -48,13 +56,81 @@ fn loadExecutable(filePath: String) -> extern "win64" fn(*mut ProcessStartInfo) 
     //read binary whole file
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
+
+    let sections = ElfParser::parse(buffer.as_slice());
+    // let mut text_buffer = Vec::new();
+    // let mut rela_dyn_buffer: Vec<u8> = Vec::new();
+    // let mut text_header = Elf64Shdr {
+    //     sh_name: 0,
+    //     sh_type: 0,
+    //     sh_flags: 0,
+    //     sh_addr: 0,
+    //     sh_offset: 0,
+    //     sh_size: 0,
+    //     sh_link: 0,
+    //     sh_info: 0,
+    //     sh_addralign: 0,
+    //     sh_entsize: 0,
+    // };
+    let mut start = i64::MAX;
+    let mut end: i64 = 0;
+
+    for section in &sections {
+        if ((section.header.sh_flags & 0x02) > 0) {
+            if ((section.header.sh_addr as i64) < start) {
+                start = section.header.sh_addr as i64;
+            }
+            if (section.header.sh_addr as i64 + section.header.sh_size as i64 > end) {
+                end = section.header.sh_addr as i64 + section.header.sh_size as i64;
+            }
+        }
+    }
+    let mut text_offset = 0;
+    let mut allocated_memory = vec![0 as u8; (end - start) as usize];
+    for section in sections {
+        // if (section.name == ".text") {
+        //     text_buffer = section.data;
+        //     text_header = section.header;
+        // } else if (section.name == ".rela.dyn") {
+        //     rela_dyn_buffer = section.data;
+        // }
+
+        if ((section.header.sh_flags & 0x02) > 0) {
+            let offset = section.header.sh_addr as usize - start as usize;
+            let size = section.header.sh_size as usize;
+            allocated_memory[offset..offset + size].copy_from_slice(&section.data);
+            if (section.name == ".text") {
+                text_offset=offset;
+
+            }
+        }
+    }
+    // let rela_dyn = rela_dyn_buffer.as_ptr() as *const u64;
+    // for i in 0..(rela_dyn_buffer.len() / 24) as isize {
+    //     unsafe {
+    //         let offset = read_volatile(rela_dyn.byte_offset(i * 24 + 0));
+    //         let info = read_volatile(rela_dyn.byte_offset(i * 24 + 8));
+    //         let addent = read_volatile(rela_dyn.byte_offset(i * 24 + 16));
+    //         if (offset >= (text_header).sh_addr
+    //             && offset < (text_header).sh_addr + text_buffer.len() as u64)
+    //         {
+    //             println!("{} -> {}", offset, addent);
+    //         }
+    //         if (addent >= (text_header).sh_addr
+    //             && addent < (text_header).sh_addr + text_buffer.len() as u64)
+    //         {
+    //             println!("{} -> {}", offset, addent);
+    //         }
+    //     }
+    // }
+
     //print file contents
 
     use windows_sys::Win32::System::Memory::{
         VirtualAlloc, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE,
     };
 
-    let size: usize = 1024 * 1024;
+    let size: usize = allocated_memory.len();
     let ptr = unsafe {
         VirtualAlloc(
             std::ptr::null_mut(),
@@ -67,16 +143,15 @@ fn loadExecutable(filePath: String) -> extern "win64" fn(*mut ProcessStartInfo) 
     //copy buffer to ptr
     let buffer_executable = ptr as *mut u8;
     unsafe {
-        std::ptr::copy_nonoverlapping(buffer.as_ptr(), buffer_executable, buffer.len());
+        std::ptr::copy_nonoverlapping(allocated_memory.as_ptr(), buffer_executable, allocated_memory.len());
     }
 
     let function_pointer: extern "win64" fn(*mut ProcessStartInfo) -> u64 = unsafe {
-        std::mem::transmute::<*mut u8, extern "win64" fn(*mut ProcessStartInfo) -> u64>(buffer_executable)
+        std::mem::transmute::<*mut u8, extern "win64" fn(*mut ProcessStartInfo) -> u64>(
+            (buffer_executable.byte_offset(text_offset as isize)),
+        )
     };
+    println!("loaded to pointer: {:X}", function_pointer as u64);
     return function_pointer;
 }
-
-
-
-
 
